0faa58753135a84d3127d7e7ad4f33f28e3367bd
songwisit <songwisit.a@gmail.com>
2015-11-15 11:12:38 +0700

    ListView Finish

--- app/src/main/java/bu/songwisit/butraffic/MainActivity.java
+++ app/src/main/java/bu/songwisit/butraffic/MainActivity.java
@@ -25,7 +25,61 @@ public class MainActivity extends AppCompatActivity {
         //Button Controller
         buttonController();
 
+        //Create ListView
+        createListView();
+
     } // Main Method
+
+    private void createListView() {
+
+        //Setup Array
+        String[] strTitle = new String[20];
+        strTitle[0] = "ห้ามเลี้ยวซ้าย";
+        strTitle[1] = "ห้ามเลี้ยวขวา";
+        strTitle[2] = "ตรงไป";
+        strTitle[3] = "เลี้ยวขวา";
+        strTitle[4] = "เลี้ยวซ้าย";
+        strTitle[5] = "ทางออก";
+        strTitle[6] = "ทางเข้า";
+        strTitle[7] = "ทางออก";
+        strTitle[8] = "หยุด";
+        strTitle[9] = "จำกัดความสุง";
+        strTitle[10] = "ทางแยก";
+        strTitle[11] = "ห้ามกลับรถ";
+        strTitle[12] = "ห้ามจอด";
+        strTitle[13] = "รถสวน";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "ทางเข้า";
+        strTitle[16] = "หยุดตรวจ";
+        strTitle[17] = "จำกัดความเร็ว";
+        strTitle[18] = "จำกัดความกว้าง";
+        strTitle[19] = "จำกัดความสุง";
+
+        int[] intIcon = {R.drawable.traffic_01,
+                R.drawable.traffic_02,
+                R.drawable.traffic_03,
+                R.drawable.traffic_04,
+                R.drawable.traffic_05,
+                R.drawable.traffic_06,
+                R.drawable.traffic_07,
+                R.drawable.traffic_08,
+                R.drawable.traffic_09,
+                R.drawable.traffic_10,
+                R.drawable.traffic_11,
+                R.drawable.traffic_12,
+                R.drawable.traffic_13,
+                R.drawable.traffic_14,
+                R.drawable.traffic_15,
+                R.drawable.traffic_16,
+                R.drawable.traffic_17,
+                R.drawable.traffic_18,
+                R.drawable.traffic_19,
+                R.drawable.traffic_20,
+        };
+
+        MyAdapter objMyAdaptor = new MyAdapter(MainActivity.this, strTitle, intIcon);
+        trafficListView.setAdapter(objMyAdaptor);
+    }
 
     private void buttonController() {
        aboutMeButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/bu/songwisit/butraffic/MyAdapter.java
+++ app/src/main/java/bu/songwisit/butraffic/MyAdapter.java
@@ -1,9 +1,12 @@
 package bu.songwisit.butraffic;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by student on 11/14/2015.
@@ -23,7 +26,7 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public int getCount() {
-        return 0;
+        return titleStrings.length;
     }
 
     @Override
@@ -38,6 +41,17 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView = objLayoutInflater.inflate(R.layout.my_listview, parent, false);
+
+        //Set Title
+        TextView titleTextView = (TextView) objView.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[position]);
+
+        //Setup Icon
+        ImageView iconImageView = (ImageView) objView.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[position]);
+
+        return objView;
     }
 }   // Main Class
